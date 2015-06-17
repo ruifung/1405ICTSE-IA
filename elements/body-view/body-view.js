1b693cab810b5e0d13a82d9b78caa5ed77cf1bda
Yip Rui Fung <ruifungyip@gmail.com>
2015-06-17 10:41:12 +0800

    BASIC GENRE FILTERING!

--- elements/body-view/body-view.js
+++ elements/body-view/body-view.js
@@ -9,6 +9,12 @@ Polymer({
             type: Boolean,
             notify: true
         },
+        pendingFilters: {
+            type: Boolean,
+            notify: true,
+            value: false,
+            observer: "_pendingFiltersObserver"
+        },
         sidebarSelected: {
             type: String,
             notify: true,
@@ -41,16 +47,15 @@ Polymer({
     },
     
     listeners: {
-        "anime-item-click": "onAnimeEvent",
-        "anime-preload": "onAnimeEvent",
-        "anime-load": "onAnimeEvent",
-        "anime-loaded": "onAnimeEvent",
-        "anime-allloaded": "onAnimeEvent"
+        "anime-item-click": "_onAnimeEvent",
+        "anime-preload": "_onAnimeEvent",
+        "anime-load": "_onAnimeEvent",
+        "anime-loaded": "_onAnimeEvent",
+        "anime-allloaded": "_onAnimeEvent"
     },
     
     ready: function() {
         this.sidebarSelected = "options";
-        this._registerButtonHandlers();
         this._updateAnimeView({status:"Currently Airing",full_page:true,year:2015});
     },
     
@@ -84,62 +89,115 @@ Polymer({
             oldV.observer.close();
         }
         var onSelectionChanged = function(changes) {
+            this.pendingFilters = true;
         }.bind(this);
         newV.observer = new ArrayObserver(newV);
         newV.observer.open(onSelectionChanged);
     },
-
-    //Event Handling
-    _registerButtonHandlers: function() {
-        this.$.backBtn.onclick = function() {
-            this._onSidebarButtonClick("options");
-            if(this.$.mainSelector.selected === "feedbackView") {
-                this.toggleClass("hidden",true,this.$.backBtn);
-                this.$.mainSelector.selected = "animeView";
-                this._toggleSidebarButtons(true);
-            } else {
-                this._onSidebarButtonClick("options");
-            }
-        }.bind(this);
-        
-        this.$.genreButton.onclick = function() {
-            this._onSidebarButtonClick("genres");
-        }.bind(this);
-        
-        this.$.seasonsButton.onclick = function() {
-            this._onSidebarButtonClick("seasons");
-        }.bind(this);
-        
-        this.$.feedbackBtn.onclick = function() {
-            this.$.mainSelector.selected = "feedbackView";
-            this.toggleClass("hidden",false,this.$.backBtn);
-            this._toggleSidebarButtons(false);
-        }.bind(this);
+    _pendingFiltersObserver: function(newV,oldV) {
+        if (newV) {
+            this.$.reloadBtn.icon = "check";
+        } else {
+            this.$.reloadBtn.icon = "refresh";
+        }
     },
-    
+
+    //HELPER FUNCTIONS
     _toggleSidebarButtons: function(toggle) {
-        this.$.genreButton.disabled = !toggle;
-        this.$.seasonsButton.disabled = !toggle;
-        this.$.sortButton.disabled = !toggle;
+        this.$.genreBtn.disabled = !toggle;
+        this.$.seasonsBtn.disabled = !toggle;
+        this.$.sortBtn.disabled = !toggle;
         this.$.showFavBtn.disabled = !toggle;
         this.$.feedbackBtn.disabled = !toggle;
     },
     
     _toggleProgressBar: function(toggle) {
+        this.loading = toggle;
         this.toggleClass("transparent",!toggle,this.$.progressBar);
     },
-
-    _onSidebarButtonClick: function(button) {
+    
+    //AIO BUTTON HANDLER.
+    _onButtonClick: function(e) {
         setTimeout(function() {
-            this.sidebarSelected = button;
+            switch(e.target.domHost.id) {
+                case "backBtn":
+                    this.sidebarSelected = "options";
+                    if(this.$.mainSelector.selected === "feedbackView") {
+                        this.toggleClass("hidden",true,this.$.backBtn);
+                        this.$.mainSelector.selected = "animeView";
+                        this._toggleSidebarButtons(true);
+                    } else {
+                        this.sidebarSelected = "options";
+                    }
+                    break;
+                case "genreBtn":
+                    this.sidebarSelected = "genres";
+                    break;
+                case "seasonsBtn":
+                    this.sidebarSelected = "seasons";
+                    break;
+                case "sortBtn":
+                    break;
+                case "showFavBtn":
+                    break;
+                case "feedbackBtn":
+                    this.$.mainSelector.selected = "feedbackView";
+                    this.toggleClass("hidden",false,this.$.backBtn);
+                    this._toggleSidebarButtons(false);
+                    break;
+                case "reloadBtn":
+                    var options = this.currentAnimeView;
+                    if (this.pendingFilters) {
+                        options = this._filterRequestOptions(options);
+                        this.pendingFilters = false;
+                    }
+                    this._updateAnimeView(options);
+                    break;
+                case "clearBtn":
+                    
+                    break;
+            }
         }.bind(this),0);
     },
-
+    
+    //AIO ANIME RELATED EVENT LISTENER.
+    _onAnimeEvent: function(e) {
+        switch(e.type) {
+            case "anime-item-click":
+                this.$.details.open(this.animeLookup[e.detail.id]);
+                break;
+            case "anime-preload":
+                this.$.progressBar.indeterminate = true;
+                this._toggleProgressBar(true);
+                break;
+            case "anime-load":
+                var progressBar = this.$.progressBar;
+                progressBar.max = this.pendingAnimeLoad;
+                progressBar.indeterminate = false;
+                break;
+            case "anime-loaded":
+                this.$.progressBar.value++;
+                break;
+            case "anime-allloaded":
+                var animeTemp = this._animesPreFilter.filter(this._filter,this);
+                this._toggleProgressBar(false);
+                this.splice.apply(this,["_animes",0,this._animes.length].concat(animeTemp));
+                console.log(this._animesPreFilter.slice(0));
+                console.log(animeTemp.slice(0));
+                console.log(this._animes.slice(0));
+                break;
+        }        
+    },
     //Main View data
-
+    
+    _filterRequestOptions: function(reqOptions) {
+        return reqOptions;
+    },
+    
     //UPDATE THE ANIME VIEW.
     _updateAnimeView: function(options) {
         this.animeLookup = {};
+        this.splice("_animesPreFilter",0,this._animesPreFilter.length);
         this.currentAnimeView = options;
         this.pendingAnimeLoad = 0;
         this.fire("anime-preload");
@@ -170,51 +228,35 @@ Polymer({
     },
     //Perform tasks when all results loaded.
     _animeDataLoaded: function(anime) {
-        this.push("_animesPreFilter",anime);
-        this.animeLookup[anime.id] = anime;
-        this.pendingAnimeLoad--;
+        if (!!this.animeLookup[anime.id] === false) {
+            this.animeLookup[anime.id] = anime;
+            this.push("_animesPreFilter",anime);
+        }
         this.fire("anime-loaded");
+        this.pendingAnimeLoad--;
         if (this.pendingAnimeLoad === 0) {
             this.fire("anime-allloaded");
         }
     },
     
+    //ANIME FILTER FUNCTION.
     _filter: function(val,ind,arr) {
+        var keep = true;
         if (this.filterAdult) {
             if (val.adult) {
                 return false;
             }
         }
-
-        return true;
-    },
-    
-    onAnimeEvent: function(e) {
-        switch(e.type) {
-            case "anime-item-click":
-                this.$.details.open(this.animeLookup[e.detail.id]);
-                break;
-            case "anime-preload":
-                this.$.progressBar.indeterminate = true;
-                this._toggleProgressBar(true);
-                break;
-            case "anime-load":
-                var progressBar = this.$.progressBar;
-                progressBar.max = this.pendingAnimeLoad;
-                progressBar.indeterminate = false;
-                break;
-            case "anime-loaded":
-                this.$.progressBar.value++;
-                break;
-            case "anime-allloaded":
-                var animeTemp = this._animesPreFilter.filter(this._filter,this);
-                
-                this._toggleProgressBar(false);
-                this.splice("_animes",0,this._animes.length);
-                this.splice.apply(this,["_animes",0,0].concat(animeTemp));
-                break;
-        }        
-    },
-    
+        if (this.selectedGenres.length !== 0) {
+            keep = false;
+            for (var i = 0; i < this.selectedGenres.length; i++) {
+                if (val.genres.indexOf(this.selectedGenres[i]) !== -1 && !keep) {
+                    keep = true;
+                    break;
+                }
+            }
+        }
+        return keep;
+    },    
 });
 /*jslint ignore: end*/
--- js/anilistAPI.js
+++ js/anilistAPI.js
@@ -18,7 +18,7 @@ function aniListAPI() {
     this.generateRequestString = function (postData) {
         var postString = "";
         for(var key in postData) {
-            postString = postString.concat(key+'='+encodeURIComponent(postData[key])+'&');
+            postString = postString.concat(key+'='+postData[key]+'&');
         }
         return postString.substring(0, postString.length - 1);
     };
